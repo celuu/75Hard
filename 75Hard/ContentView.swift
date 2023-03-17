40589af028c6a80126f07492eaa23346d0726376
celuu <christine.e.luu@gmail.com>
2023-03-17 15:47:59 -0500

    first commit

---
+++ 75Hard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  75Hard
+//
+//  Created by Christine Luu on 3/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+            Text("Hi Christine")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 75Hard/_5HardApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5HardApp.swift
+//  75Hard
+//
+//  Created by Christine Luu on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct _5HardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
